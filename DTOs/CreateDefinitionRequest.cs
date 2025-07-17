e35550800b520e88aa2cbe59f2a842568746582f
Nilabrata1 <nilabrata2003@gmail.com>
2025-07-18 01:55:00 +0530

    Update CreateDefinitionRequest.cs

--- DTOs/CreateDefinitionRequest.cs
+++ DTOs/CreateDefinitionRequest.cs
@@ -1,11 +1,13 @@
+// File: DTOs/CreateDefinitionRequest.cs
 using WorkflowEngine.Models;
 
-namespace WorkflowEngine;
-
-public class CreateDefinitionRequest
+namespace WorkflowEngine.DTOs
 {
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public List<State> States { get; set; } = new();
-    public List<TransitionAction> Actions { get; set; } = new();
+    public class CreateDefinitionRequest
+    {
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+        public List<State> States { get; set; } = new();
+        public List<TransitionAction> Actions { get; set; } = new();
+    }
 }
